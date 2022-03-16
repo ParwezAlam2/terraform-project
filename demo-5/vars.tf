2d79ccd5773020e32087e91a462bbc6aea7c34c2
ParwezAlam2 <99421905+ParwezAlam2@users.noreply.github.com>
2022-03-16 15:22:23 +0530

    Add files via upload

---
+++ demo-5/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    ap-south-1 = "ami-0f2e255ec956ade7f"
+  }
+}
+
